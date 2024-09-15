da6546637ef7dab6e730e02e5a664cbb41de8bd4
Karthik S <karthiani000@gmail.com>
2024-09-15 21:50:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend", "HTML5", "CSS3", "JavaScript", "React"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
